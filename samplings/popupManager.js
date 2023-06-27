f044af929f1b433ac8d28cd2ea96bb8fd27bb267
anthoDev35 <arcaniafr@gmail.com>
2023-06-27 23:06:00 +0200

    multiples changes

--- samplings/popupManager.js
+++ samplings/popupManager.js
@@ -1,139 +1,219 @@
 class PopupManager {
     constructor() {
         this.popup = document.querySelector('.popup');
+        this.popupHeader = document.querySelector('.popup-header');
         this.popupContent = document.querySelector('.popup-content');
         this.selectedOption = null;
-        this.forms = [
-            {
-                option: 'Option 1',
-                fields: [
-                    { type: 'input', label: 'Field 1' },
-                    {
-                        type: 'select',
-                        label: 'Field 2',
-                        options: [
-                            { value: 'Option A', label: 'Option A', form: [{ type: 'input', label: 'Subfield 1' }] },
-                            { value: 'Option B', label: 'Option B', form: [{ type: 'input', label: 'Subfield 2' }] },
-                            { value: 'Option C', label: 'Option C', form: [{ type: 'input', label: 'Subfield 3' }] }
-                        ]
-                    },
-                    { type: 'checkbox', label: 'Field 3', options: ['Option 1', 'Option 2', 'Option 3'] }
-                ]
-            },
-            {
-                option: 'Option 2',
-                fields: [
-                    { type: 'radio', label: 'Field A', options: ['Option X', 'Option Y', 'Option Z'] },
-                    { type: 'input', label: 'Field B' },
-                    { type: 'select', label: 'Field C', options: ['Option I', 'Option II', 'Option III'] }
-                ]
-            },
-            // Ajoutez les autres options avec leurs champs ici
-        ];
+        // can contruct this from the backend
+        this.forms = [{
+            optionId: 0,
+            optionTitle: 'Add widget',
+            optionClass: 'popup-header',
+            fields: [
+                { tag: 'input', type: 'hidden', name: 'widgetId', id: 'widgetId', value: '321' },
+                { tag: 'input', type: 'hidden', name: 'minWidth', id: 'minWidth', value: '6' },
+                { tag: 'input', type: 'hidden', name: 'minHeight', id: 'minHeight', value: '2' },
+                { tag: 'input', type: 'hidden', name: 'selectType', id: 'selectType', value: 'option2' },
+                { tag: 'input', type: 'hidden', name: 'selectFile', id: 'selectFile', value: '123456.pdf' },
+                {
+                    tag: 'div', type: 'none', name: 'none', title: 'Select a type', class: 'button-type', options: [
+                        {
+                            title: 'Option 1',
+                            id: 'option_id_1',
+                            icon: 'fas fa-clock',
+                            subForm: [
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                                { tag: 'input', type: 'text', name: 'subfield_2', title: 'Subfield 2', id: 'subfield_2' },
+                                { tag: 'select', name: 'subfield_3', title: 'Subfield 3', id: 'subfield_3', options: [{value: 1, title:'Option A'},{ value: 2, title:'Option B'},{ value: 3, title:'Option C'}]},
+                            ]
+                        },
+                        {
+                            title: 'Option 2',
+                            id: 'option_id_2',
+                            icon: 'fas fa-check',
+                            subForm: [
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                                { tag: 'input', type: 'text', name: 'subfield_2', title: 'Subfield 2', id: 'subfield_2' },
+                                { tag: 'select', title: 'Subfield 4', name: 'subfield_4', label: 'Subfield 4', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}]},
+                            ]
+                        },
+                        {
+                            title: 'Option 3',
+                            id: 'option_id_3',
+                            icon: 'fas fa-calendar',
+                            subForm: [
+                                { tag: 'input', type: 'text', name: 'subfield_2', title: 'Subfield 2', id: 'subfield_2' },
+                                { tag: 'checkbox', name: 'subfield_3', title: 'Subfield 3', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}] },
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                            ]
+                        },
+                        {
+                            title: 'Option 4',
+                            id: 'option_id_4',
+                            icon: 'fas fa-cogs',
+                            subForm: [
+                                { tag: 'select', title: 'Subfield 4', name: 'subfield_4', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}]},
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                            ]
+                        },
+                        {
+                            title: 'Option 5',
+                            id: 'option_id_5',
+                            icon: 'fas fa-file',
+                            subForm: [
+                                { tag: 'select', title: 'Subfield 4', name: 'subfield_4', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}]},
+                                { tag: 'select', title: 'Subfield 4', name: 'subfield_4', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}]},
+                                { tag: 'select', title: 'Subfield 4', name: 'subfield_4', options: [{value: 4, title:'Option D'},{ value: 5, title:'Option E'},{ value: 6, title:'Option F'}]},
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                            ]
+                        },
+                        {
+                            title: 'Option 6',
+                            id: 'option_id_6',
+                            icon: 'fas fa-envelope',
+                            subForm: [
+                                { tag: 'input', type: 'text', name: 'subfield_1', title: 'Subfield 1', id: 'subfield_1' },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }];
     }
 
-    openPopup(option) {
-        // Affiche le pop-up avec le formulaire correspondant à l'option choisie
-        this.selectedOption = option;
+    openPopup(options = 0) {
+        this.selectedOption = options;
         this.popup.style.display = 'block';
         this.popupContent.innerHTML = '';
-
-        const selectedForm = this.forms[option - 1];
+    
+        const selectedForm = this.forms[options];
         const fields = selectedForm.fields;
-
-        const formTitle = document.createElement('h2');
-        formTitle.textContent = selectedForm.option;
-        this.popupContent.appendChild(formTitle);
-
+    
+        this.popupHeader.textContent = selectedForm.optionTitle;
+        this.popupHeader.className = selectedForm.optionClass;
+    
         const form = document.createElement('form');
-
-        fields.forEach((field, index) => {
-            const label = document.createElement('label');
-            label.textContent = field.label + ' :';
-            form.appendChild(label);
-
-            if (field.type === 'input') {
+    
+        fields.forEach(field => {
+            if (field.tag === 'input') {
+                const label = document.createElement('label');
+                label.textContent = field.title + ' :';
+    
                 const input = document.createElement('input');
-                input.type = 'text';
-                input.name = 'field' + (index + 1);
-                form.appendChild(input);
-            } else if (field.type === 'select') {
-                const select = document.createElement('select');
-                select.name = 'field' + (index + 1);
-
+                input.type = field.type;
+                input.name = field.name;
+                input.value = field.value;
+                if(field.type == 'hidden') form.appendChild(input);
+                else form.append(label, input);
+            } else if (field.tag === 'div') {
+                const label = document.createElement('label');
+                label.textContent = field.title + ' :';
+    
+                const div = document.createElement('div');
+                div.className = field.class;
+    
                 field.options.forEach(option => {
-                    const optionElement = document.createElement('option');
-                    optionElement.value = option.value;
-                    optionElement.textContent = option.label;
-                    select.appendChild(optionElement);
+                    const optionElement = document.createElement('label');
+                    optionElement.className = 'select_type_option';
+                    optionElement.id = option.id;
+                    optionElement.title = option.title;
+    
+                    const icon = document.createElement('i');
+                    icon.className = option.icon;
+    
+                    optionElement.appendChild(icon);
+                    optionElement.appendChild(document.createTextNode(option.title));
+    
+                    optionElement.addEventListener('click', () => {
+                        if (option.subForm) {
+                            const all_options_id = document.querySelectorAll('[id^="option_id_"]');
+                            all_options_id.forEach(all_option =>{
+                                const option_id = document.getElementById(all_option.id);
+                                option_id.classList.remove('active');
+                            });
+                            const option_id = document.getElementById(option.id);
+                            if(option_id) option_id.classList.add('active');
+                            this.renderSubForm(form, option.subForm);
+                        } else {
+                            this.clearSubForm(form);
+                        }
+                    });
+    
+                    div.appendChild(optionElement);
                 });
-
-                select.addEventListener('change', event => {
-                    const selectedOption = field.options.find(option => option.value === event.target.value);
-                    if (selectedOption && selectedOption.form) {
-                        this.renderSubForm(form, selectedOption.form);
-                    } else {
-                        this.clearSubForm(form);
-                    }
-                });
-
-                form.appendChild(select);
-            } else if (field.type === 'checkbox' || field.type === 'radio') {
-                field.options.forEach(option => {
-                    const input = document.createElement('input');
-                    input.type = field.type;
-                    input.name = 'field' + (index + 1);
-                    input.value = option;
-
-                    const optionLabel = document.createElement('label');
-                    optionLabel.textContent = option;
-                    optionLabel.appendChild(input);
-
-                    form.appendChild(optionLabel);
-                });
+    
+                form.append(label, div);
             }
-
-            form.appendChild(document.createElement('br'));
         });
-
+    
         const submitButton = document.createElement('button');
         submitButton.textContent = 'Valider';
         submitButton.addEventListener('click', (event) => {
-            this.submitForm(event); // Appelle la fonction submitForm() en passant l'événement en paramètre
+            this.submitForm(event);
         });
         form.appendChild(submitButton);
-
+    
         this.popupContent.appendChild(form);
     }
 
     renderSubForm(form, subFormFields) {
         let subFormContainer = form.querySelector('.subform-container');
-    
+        
         if (!subFormContainer) {
             subFormContainer = document.createElement('div');
             subFormContainer.classList.add('subform-container');
             form.appendChild(subFormContainer);
         }
-    
-        subFormContainer.innerHTML = ''; // Réinitialise le contenu du sous-formulaire
+        subFormContainer.innerHTML = '';
     
         subFormFields.forEach((field, index) => {
             const label = document.createElement('label');
-            label.textContent = field.label + ' :';
+            label.textContent = field.title + ' :';
             subFormContainer.appendChild(label);
     
-            if (field.type === 'input') {
+            if (field.tag === 'input') {
                 const input = document.createElement('input');
                 input.type = 'text';
                 input.name = 'subfield' + (index + 1);
                 subFormContainer.appendChild(input);
+           
+            } else if (field.tag === 'select') {
+                const label = document.createElement('label');
+                label.textContent = field.title + ' :';
+                const select = document.createElement('select');
+                select.name = field.name;
+    
+                field.options.forEach(option => {
+                    const optionElement = document.createElement('option');
+                    optionElement.value = option.value;
+                    optionElement.textContent = option.title;
+                    select.appendChild(optionElement);
+                });
+    
+                subFormContainer.append(label, select);
+            } else if (field.tag === 'checkbox' || field.tag === 'radio') {
+                const label = document.createElement('label');
+                label.textContent = field.title + ' :';
+                subFormContainer.appendChild(label);
+                field.options.forEach(option => {
+                    const input = document.createElement('input');
+                    input.type = field.tag;
+                    input.name = field.name;
+                    input.value = option.value;
+    
+                    const optionLabel = document.createElement('label');
+                    optionLabel.textContent = option.title;
+                    optionLabel.appendChild(input);
+    
+                    subFormContainer.appendChild(optionLabel);
+                });
             }
     
             subFormContainer.appendChild(document.createElement('br'));
         });
     
         const submitButton = form.querySelector('button');
-        form.insertBefore(subFormContainer, submitButton); // Insère le sous-formulaire avant le bouton "Valider"
+        form.insertBefore(subFormContainer, submitButton);
     }
 
     clearSubForm(form) {
@@ -144,22 +224,18 @@ class PopupManager {
     }
     
     submitForm(event) {
-        event.preventDefault(); // Empêche le comportement par défaut de l'événement de soumission du formulaire
+        event.preventDefault();
     
-        // Récupère les valeurs des champs du formulaire et effectue le traitement nécessaire
         const form = this.popupContent.querySelector('form');
         const formData = new FormData(form);
     
-        const values = {};
+        const request = {};
         for (const [name, value] of formData) {
-            values[name] = value;
+            request[name] = value;
         }
+        
+        console.log('Request :', request);
     
-        // Affiche les valeurs dans la console (vous pouvez les traiter comme vous le souhaitez)
-        console.log('Option : ' + this.forms[this.selectedOption - 1].option);
-        console.log('Champs :', values);
-    
-        // Ferme le pop-up
         this.popup.style.display = 'none';
         form.reset();
         this.clearSubForm(form);
